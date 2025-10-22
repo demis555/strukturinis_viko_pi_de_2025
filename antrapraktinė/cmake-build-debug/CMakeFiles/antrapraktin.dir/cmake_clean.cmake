1ed63c579aa5b54130a938ca45d17db97ef40443
demis555 <demis1264@protonmai.com>
2025-10-22 09:31:16 +0300

    prideta antros praktines kopija su veikianciu cmake (antraprakt)

---
+++ antrapraktinė/cmake-build-debug/CMakeFiles/antrapraktin.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/antrapraktin.dir/main.cpp.obj"
+  "CMakeFiles/antrapraktin.dir/main.cpp.obj.d"
+  "antrapraktin.exe"
+  "antrapraktin.exe.manifest"
+  "antrapraktin.pdb"
+  "libantrapraktin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/antrapraktin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ antrapraktinė/cmake-build-debug/CMakeFiles/antrapraktin.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for antrapraktin.
+# This may be replaced when dependencies are built.
